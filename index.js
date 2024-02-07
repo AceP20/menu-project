d6e6bea553a17bcf60502d381235e55be4eec247
AceP20 <alvaro.polanco20@gmail.com>
2024-02-07 14:13:25 -0500

    render and menu tranfer on JS completed

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import menuArray from '/data.js'
 
-console.log(menuArray)
+const menuOptions = []
+
+menuArray.forEach(item => {
+    menuOptions.push(
+        `<h2>${item.emoji}</h2>
+        <h4>${item.name}</h4>
+        <p>${item.ingredients}</p>
+        <h4>${item.price}</h4>`
+    )
+}); 
+
+function renderMenu(menu){
+    document.getElementById('menu').innerHTML = menu
+}
+
+renderMenu(menuOptions.join(''))
+
+// we need to fix the layout of the render in the main page
+// also need to set the horizontal line divider
