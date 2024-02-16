92b93187bb650c0eaa42001cf2a6027cc27d2f57
AceP20 <alvaro.polanco20@gmail.com>
2024-02-16 14:59:43 -0500

    added function dataset to plus signs

--- data.js
+++ data.js
@@ -22,5 +22,5 @@ export const menuArray = [
     }
 ]
 
-export const customerOrder = ["testing"]
+export const customerOrder = []
 
--- index.js
+++ index.js
@@ -2,22 +2,27 @@ import { menuArray, customerOrder } from '/data.js'
 
 const menuOptions = []
 
+//goes through each item in array to return visual HTML code for food menu card
 menuArray.forEach(item => {
     menuOptions.push(`
-        <div class="menu-card">
-        <h2>${item.emoji}</h2>
-        <spam>
-        <h4>${item.name}</h4>
-        <p>${item.ingredients}</p>
-        <h4>$${item.price}</h4>
-        </spam><i class="fa-solid fa-plus"></i></div>`
+    <div class="menu-card">
+    <h2>${item.emoji}</h2>
+    <spam>
+    <h4>${item.name}</h4>
+    <p>${item.ingredients}</p>
+    <h4>$${item.price}</h4>
+    </spam><i data-item='${item.name}' class="fa-solid fa-plus"></i></div>`
     )
 }); 
 
 function renderMenu(menu){
     document.getElementById('menu').innerHTML = menu
 }
-
+ 
 renderMenu(menuOptions.join(''))
 
 //create Your order bottom of the page
+
+document.addEventListener('click', (e)=>{
+    console.log(e.target.dataset.item)
+})
