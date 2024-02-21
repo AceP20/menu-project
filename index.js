b3fbb1be7f409614885a713f26618e9501b9c1c2
AceP20 <alvaro.polanco20@gmail.com>
2024-02-20 22:31:43 -0500

    Continued work on customer order display

--- index.js
+++ index.js
@@ -7,33 +7,63 @@ menuArray.forEach(item => {
     menuOptions.push(`
     <div class="menu-card">
     <h2>${item.emoji}</h2>
-    <spam>
+    <span>
     <h4>${item.name}</h4>
     <p>${item.ingredients}</p>
     <h4>$${item.price}</h4>
-    </spam><i data-item='${item.name}' class="fa-solid fa-plus"></i></div>`
+    </span><i data-price='${item.price}' data-name='${item.name}' class="fa-solid fa-plus"></i></div>`
     )
-}); 
+});
 
-function renderMenu(menu){
-    document.getElementById('menu').innerHTML = menu
+function renderMenu(menu) {
+    document.querySelector('#menu').innerHTML = menu
 }
- 
+
 renderMenu(menuOptions.join(''))
 
 // adds whichever element is clicked to customer order array
-document.addEventListener('click', (e)=>{
-    if (e.target.dataset.item){
-        customerOrderArr.push((e.target.dataset.item))
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.name) {
+        customerOrderArr.push(e.target.dataset)
+        renderCustomerOrder(e.target.dataset)
+    } 
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove-btn')){
+        console.log(customerOrderArr)
+        if(customerOrderArr.includes(e.target.dataset.item)){
+            console.log('working')
+        }
     }
 })
 
-document.getElementById('testBtn').addEventListener('click', ()=>{
+const customerOrderHTMLArr = []
+
+const renderCustomerOrder = (itemsOrdered) =>{
+    customerOrderHTMLArr.push(`
+    <span><p>${itemsOrdered.name}</p><button data-item='${itemsOrdered.name}' class='remove-btn'>(remove)</button><p class='price-display'>$${itemsOrdered.price}</p></span>
+    `
+    )
+    document.querySelector('#customer-order').innerHTML = customerOrderHTMLArr.join(' ')
+    
+    //adds up total price and updates the HTML on site
+    const totalPrice = customerOrderArr.reduce((sum, currentItem )=> sum + Number(currentItem.price), 0)
+    document.querySelector('#total-price-display').innerHTML = `Total Price: <span>$${totalPrice}</span>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#testBtn').addEventListener('click', () => {
     console.log(customerOrderArr)
 })
 
-if (customerOrderArr.length === 0){
-    document.getElementById('customer-order').style.display = 'none'
-} 
-
-// try to fix the display when customerOrderArr is empty compared to when has elements in it
